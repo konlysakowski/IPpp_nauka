421245fc2b801c75917317c72a729650099e5977
konlysakowski <kl310675@student.polsl.pl>
2024-06-18 15:48:04 +0200

    kolT0B_przykladowe

--- main.cpp
+++ main.cpp
@@ -1,22 +1,86 @@
-#include "funkcje.h"
+#include <iostream>
 
+using namespace std;
+
+enum class Aktywnosc
+{
+	aktywny,
+	nieaktywny
+};
+
+
+class Klient
+{
+	char nazwaKlienta[50];
+};
+
+class KontoBankowe
+{
+protected:
+	int numerKonta;
+	double stanKonta;
+	Aktywnosc aktywnosc;
+	Klient* wlasciciel; 
+	static int aktywneKonta;
+public:
+	void setNumerKonta(int numer) { numerKonta = numer; }
+	void setStanKonta(double stan) { stanKonta = stan; }
+	void setAktywnosc(Aktywnosc _aktywnosc) { aktywnosc = _aktywnosc; }
+	void setWlasciciel(Klient* _wlasciciel) { wlasciciel = _wlasciciel; }
+
+	int getNumerKonta() { return numerKonta; }
+	double getStanKonta() { return stanKonta; }
+	Aktywnosc getAktywnosc() { return aktywnosc; }
+	Klient* getWlasciciel() { return wlasciciel; }
+
+	KontoBankowe(int numer, double stan, Aktywnosc aktyw, Klient* wlasc)
+		:numerKonta(numer), stanKonta(stan), aktywnosc(aktyw), wlasciciel(wlasc)
+	{
+		if (aktywnosc == Aktywnosc::aktywny)
+		{
+			aktywneKonta++;
+		}
+	}
+
+	~KontoBankowe()
+	{
+		aktywneKonta--;
+	}
+
+	virtual void prognoza()
+	{
+		cout << "Prognozowany stan konta: " << stanKonta * 0 + stanKonta << endl;
+	}
+
+};
+
+class KontoOszczednosciowe : public KontoBankowe
+{
+private:
+	double stopaOprocentowania; 
+public:
+	void setStopaOprocentowania(double stopa) { stopaOprocentowania = stopa; }
+	
+	double getStopaOprocentowania() { return stopaOprocentowania; }
+
+	KontoOszczednosciowe(int numer, double stan, Aktywnosc aktyw, Klient* wlasc, double stopa)
+		:KontoBankowe(numer, stan, aktyw, wlasc)
+	{
+		if (stopa > 0)
+		{
+			setStopaOprocentowania(stopa);
+		}
+	}
+
+	void prognoza() override
+	{
+		cout << "Prognozowany stan konta: " << stanKonta * stopaOprocentowania + stanKonta << endl;
+	}
+
+};
 
 int main()
 {
-	int numRows = 3, numCols = 5;
-	auto tab = alokacja(numRows, numCols);
-	
-	zapis("pliczek.txt", tab, numRows, numCols);
-	
-	auto tab_2 = odczyt("pliczek.txt", numRows, numCols);
 
-	for (int i = 0; i < numRows; i++)
-	{
-		for (int j = 0; j < numCols; j++)
-		{
-			cout << tab_2[i][j] << " ";
-		}
-		cout << endl;
-	}
 }
 
